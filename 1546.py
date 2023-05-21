25f7b5252b67f777d4cb278b204a753205db405f
chrisbaek <chriskai007@gmail.com>
2023-05-21 22:12:30 +0900

    Add files via upload

--- 1546.py
+++ 1546.py
@@ -1,8 +1,26 @@
-
 N=int(input())
-score=[]
+
+score=list(map(int, input().split()))
+
+if N!=len(score):
+   print("다시 입력해주세요")
+   score=list(map(int, input().split()))
+
+new_score=[]
+for i in range(N):
+    new_score.append(0)
 
 for i in range(N):
-   score=input().split()
+    new_score[i]=score[i]/max(score)*100
 
-print(score)
+sum = 0
+
+for i in range(N):
+    sum = sum + new_score[i]
+
+average=sum/N
+
+print(round(average,5))
+
+
+
